4f01afb7be17a519507626e4d61e618548060d4d
Karina <karinadesyatnik27@gmail.com>
2023-05-05 01:52:05 +0300

    button

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
-
+    jQuery(document).ready(($) => {
+        $('.quantity').on('click', '.plus', function(e) {
+            let $input = $(this).prev('input.qty');
+            let val = parseInt($input.val());
+            $input.val( val+1 ).change();
+        });
+ 
+        $('.quantity').on('click', '.minus', 
+            function(e) {
+            let $input = $(this).next('input.qty');
+            var val = parseInt($input.val());
+            if (val > 0) {
+                $input.val( val-1 ).change();
+            } 
+        });
+    });
 
 });
